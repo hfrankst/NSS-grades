74e652b0eb50103e7d9b65da62836d101019a998
Harper Frankstone <Harper Frankstone>
2017-01-23 14:09:02 -0600

    started on the student grades exercise

---
+++ grades.js
@@ -0,0 +1,25 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+var lowGrade;
+var highGrade;
+
+for (i = 0; i < scores.length; i++){
+	if(scores.value > 50 && scores.value < 60) {
+		console.log("");
+	} else {
+		if(scores.value > 61 && scores.value < 70) {
+
+		} else {
+			if(scores.value > 71 && scores.value < 80) {
+
+			} else {
+				if(scores.value > 81 && scores.value < 90) {
+
+				} else {
+					if(scores.value > 91 && scores.value < 100) {
+
+					}
+				}
+			}
+		}
+	}
+}
